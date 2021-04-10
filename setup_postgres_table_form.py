396dcd2bb5d3678110c4fe5d1ce2bb023cd7796d
viperior <viperior@gmail.com>
2021-04-10 15:52:04 -0700

    Add script to create the form table in postgres

---
+++ setup_postgres_table_form.py
@@ -0,0 +1,34 @@
+import json
+import psycopg2
+
+def main():
+    try:
+        with open('config.json', 'r') as config_file:
+            config_data = json.load(config_file)
+
+        connection = psycopg2.connect(
+            host = config_data['postgres_host'],
+            database = config_data['postgres_database'],
+            user = config_data['postgres_username'],
+            password = config_data['postgres_password']
+        )
+
+        cursor = connection.cursor()    
+        sql = """
+            CREATE TABLE IF NOT EXISTS form (
+                form_id bigserial PRIMARY KEY,
+                form_schema text NOT NULL,
+                entity_name text NOT NULL
+            );
+        """
+        cursor.execute(sql)
+        connection.commit()
+        cursor.close()
+        connection.close()
+    except Exception as ex:
+        print('Error occurred while attempting to connect to postgres database.')
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
+
+main()
