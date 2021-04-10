a56b03ad4e4bd564ed3b44b794fa68a5d1714490
viperior <viperior@gmail.com>
2021-04-10 16:01:42 -0700

    Remove self-call from setup_postgres_database script

--- setup_postgres_database.py
+++ setup_postgres_database.py
@@ -25,5 +25,3 @@ def main():
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
         message = template.format(type(ex).__name__, ex.args)
         print(message)
-
-main()
