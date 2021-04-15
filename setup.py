df73958865289cb69358909729d86b95554f824a
viperior <viperior@gmail.com>
2021-04-15 07:54:23 -0700

    Instantiate an instance of the DatabaseConnection class

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import database_connection
 
 def main():
-    npb_db_connection = DatabaseConnection()
+    npb_db_connection = database_connection.DatabaseConnection()
     npb_db_connection.setup_database()
 
 main()
