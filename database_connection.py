26ae5eee6c33b131aaa26dec1cccdbe30850453a
viperior <viperior@gmail.com>
2021-04-15 07:06:44 -0700

    Move database installation logic to DatabaseConnection class

--- database_connection.py
+++ database_connection.py
@@ -6,8 +6,48 @@ class DatabaseConnection:
         self.schema = self.config['database_schema']
         self.password = self.config['database_password']
 
+    def execute_sql(sql):
+        try:
+            connection = psycopg2.connect(
+                host = self.host,
+                user = self.username,
+                password = self.password
+            )
+
+            autocommit = psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT
+            connection.set_isolation_level(autocommit)
+            cursor = connection.cursor()    
+            cursor.execute(sql)
+            connection.commit()
+            cursor.close()
+            connection.close()
+        except Exception as ex:
+            print('Error occurred while attempting to connect to database host: ', self.host)
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+
     def get_config():
         with open('config.json', 'r') as input_file:
             config_data = json.load(input_file)
 
         return config_data
+
+    def setup_database():
+        setup_database_schema()
+        setup_database_tables()
+
+    def setup_database_schema()
+        execute_sql(f"CREATE DATABASE {self.schema};")
+
+    def setup_database_tables():
+        sql = """
+            CREATE TABLE IF NOT EXISTS form (
+                form_id bigserial PRIMARY KEY,
+                return_s3_doc_id bigint NOT NULL,
+                return_version text NOT NULL,
+                ein bigint NOT NULL,
+                return_filer_name text NOT NULL
+            );
+        """
+        execute_sql(sql)
--- setup_database.py
+++ setup_database.py
@@ -1,31 +1,5 @@
-import json
-import psycopg2
+import database_connection
 
 def main():
-    host = config_data['database_host'],
-    user = config_data['database_username'],
-    password = config_data['database_password']
-    
-    try:
-        with open('config.json', 'r') as config_file:
-            config_data = json.load(config_file)
-
-        connection = psycopg2.connect(
-            host = host,
-            user = user,
-            password = password
-        )
-
-        autocommit = psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT
-        connection.set_isolation_level(autocommit)
-        cursor = connection.cursor()    
-        sql = """CREATE DATABASE npb;"""
-        cursor.execute(sql)
-        connection.commit()
-        cursor.close()
-        connection.close()
-    except Exception as ex:
-        print('Error occurred while attempting to connect to database host: ', host)
-        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-        message = template.format(type(ex).__name__, ex.args)
-        print(message)
+    npb_db_connection = DatabaseConnection()
+    npb_db_connection.setup_database()
