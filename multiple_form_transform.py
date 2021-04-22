a2cb3a30b1d724105dbd4dbc1c4b47337c74095d
viperior <viperior@gmail.com>
2021-04-22 16:41:00 -0700

    Store the current filing data in the database

--- multiple_form_transform.py
+++ multiple_form_transform.py
@@ -1,3 +1,4 @@
+import database_connection
 import os
 import return_filing
 
@@ -5,6 +6,7 @@ def main():
     limit = 5
     directory = 'data\\indices'
     file_list = os.scandir(directory)
+    connection = database_connection.DatabaseConnection()
 
     for index, form_path in enumerate(file_list):
         if index > limit:
@@ -13,5 +15,6 @@ def main():
             full_form_path = directory + "\\" + form_path.name
             current_return = return_filing.ReturnFiling(form_path.name, full_form_path)
             current_return.display_contents()
+            connection.insert_single_record(current_return.get_database_payload())
 
 main()
