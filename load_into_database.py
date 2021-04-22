5866d56b2c1582407e6271082678c506f001be5c
viperior <viperior@gmail.com>
2021-04-22 12:48:18 -0700

    Move payload formation logic to ReturnFiling class

--- load_into_database.py
+++ load_into_database.py
@@ -14,13 +14,6 @@ def main():
         else:
             current_return = return_filing.ReturnFiling(form.name, form.path)
             print(f"entity_name: {current_return.get_return_filer_name_full()}")
-            payload = {
-                "form_file_name": form.name,
-                "return_s3_doc_id": current_return.return_s3_doc_id,
-                "return_version": current_return.get_return_version(),
-                "ein": current_return.get_return_filer_ein(),
-                "return_filer_name": current_return.get_return_filer_name_full()
-            }
-            store_form_to_db(payload)
+            store_form_to_db(current_return.get_database_payload())
 
 main()
