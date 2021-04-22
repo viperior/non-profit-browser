b7624aea49450269fdd74bc7b2f65c22fe681221
viperior <viperior@gmail.com>
2021-04-22 16:42:58 -0700

    Add option to connect without defining a schema and use that mode for uninstalling and installing the database; add record insertion function

--- database_connection.py
+++ database_connection.py
@@ -9,13 +9,12 @@ class DatabaseConnection:
         self.schema = self.config['database_schema']
         self.password = self.config['database_password']
 
-    def execute_sql(self, sql):
+    def execute_sql(self, sql, use_schema=True):
         try:
-            connection = psycopg2.connect(
-                host = self.host,
-                user = self.username,
-                password = self.password
-            )
+            if use_schema:
+                connection = self.get_connection_with_schema()
+            else:
+                connection = self.get_connection_schemaless()
 
             autocommit = psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT
             connection.set_isolation_level(autocommit)
@@ -23,18 +22,69 @@ class DatabaseConnection:
             cursor.execute(sql)
             connection.commit()
             cursor.close()
-            connection.close()
-        except Exception as ex:
-            print('Error occurred while attempting to connect to database host: ', self.host)
+        except SyntaxError as ex:
+            print('[ERROR] Syntax error occurred while attempting to execute '\
+                'sql statement')
             template = "An exception of type {0} occurred. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
             print(message)
+        except Exception as ex:
+            print(
+                'Error occurred while attempting to connect to database host: ',
+                self.host
+            )
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+        finally:
+            connection.close()
+
+    def get_connection_schemaless(self):
+        connection = psycopg2.connect(
+            host = self.host,
+            user = self.username,
+            password = self.password
+        )
+        return connection
+
+    def get_connection_with_schema(self):
+        connection = psycopg2.connect(
+            host = self.host,
+            dbname = self.schema,
+            user = self.username,
+            password = self.password
+        )
+        return connection
 
     def get_config(self):
         with open('config.json', 'r') as input_file:
             config_data = json.load(input_file)
 
         return config_data
+
+    def insert_single_record(self, payload):
+        keys = [
+            'return_s3_doc_id',
+            'return_version',
+            'ein',
+            'return_filer_name'
+        ]
+        sql = 'INSERT INTO form ('
+
+        for i, key in enumerate(keys):
+            sql += key
+
+            if i < len(keys) - 1:
+                sql += ','
+
+        sql += ") VALUES ("
+        sql += str(payload['return_s3_doc_id']) + ','
+        sql += "'" + payload['return_version'] + "',"
+        sql += str(payload['ein']) + ','
+        sql += "'" + payload['return_filer_name'] + "'"
+        sql += ");"
+        print(sql)
+        self.execute_sql(sql)
 
     def setup_database(self):
         self.uninstall_database()
@@ -43,10 +93,13 @@ class DatabaseConnection:
         print('[SUCCESS] Non-Profit Browser database installation complete!')
 
     def setup_database_schema(self):
-        self.execute_sql(f"CREATE DATABASE {self.schema};")
+        self.execute_sql(
+            sql = f"CREATE DATABASE {self.schema};",
+            use_schema = False
+        )
 
     def setup_database_tables(self):
-        sql = """
+        sql = f"""
             CREATE TABLE IF NOT EXISTS form (
                 form_id bigserial PRIMARY KEY,
                 return_s3_doc_id bigint NOT NULL,
@@ -58,4 +111,14 @@ class DatabaseConnection:
         self.execute_sql(sql)
 
     def uninstall_database(self):
-        self.execute_sql(f"DROP DATABASE IF EXISTS {self.schema};")
+        self.uninstall_table_form()
+        self.uninstall_database_schema()
+
+    def uninstall_database_schema(self):
+        self.execute_sql(
+            sql = f"DROP DATABASE IF EXISTS {self.schema};",
+            use_schema = False
+        )
+
+    def uninstall_table_form(self):
+        self.execute_sql(f"DROP TABLE IF EXISTS form;")
