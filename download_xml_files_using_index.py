95ad8ea9d4f5cc4b67fa8150281b7a8bfb49edf8
viperior <viperior@gmail.com>
2021-04-27 18:40:27 -0700

    Use a multiprocessing pool to distribute and process the index partitions

--- download_xml_files_using_index.py
+++ download_xml_files_using_index.py
@@ -1,35 +1,25 @@
 import json
-import progressbar
+import multiprocessing
 import requests
 
-def main():
-    file_count_estimate = 4000000
-    file_counter = 0
+def process_index_partition(index_partition):
+    for doc in index_partition:
+        url = f"https://s3.amazonaws.com/irs-form-990/{doc}"
+        r = requests.get(url)
 
+        if r.status_code == 200:
+            with open(f"data/npb-xml/{doc}", 'w') as output_file:
+                output_file.write(r.text)
+        else:
+            print(f"[ERROR] Error encountered while attempting to "\
+                "download {doc}")
+
+def main():
     with open('data/npb-indices/npb_full_index.json', 'r') as index_file:
         index = json.load(index_file)
     
-    with progressbar.ProgressBar(max_value=file_count_estimate) as bar:
-        for index_partition in index:
-            for doc in index_partition:
-                url = f"https://s3.amazonaws.com/irs-form-990/{doc}"
-                r = requests.get(url)
-
-                if r.status_code == 200:
-                    with open(f"data/npb-xml/{doc}", 'w') as output_file:
-                        output_file.write(r.text)
-                else:
-                    print(f"[ERROR] Error encountered while attempting to "\
-                        "download {doc}")
-
-                file_counter += 1
-
-                if file_counter <= file_count_estimate:
-                    bar.update(file_counter)
-                else:
-                    printf(f"[INFO] Estimated file count of "\
-                        "{file_count_estimate} exceeded with a final count "\
-                        "of: {file_counter}")
+    with multiprocessing.Pool() as pool:
+        pool.map(process_index_partition, index)
 
 if __name__ == '__main__':
     main()
